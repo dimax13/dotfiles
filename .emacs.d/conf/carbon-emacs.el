7836fb708c21c37041d97c46605c53fa182aafcb
dimax13 <dimax13@macsbook-air.local>
2012-10-18 17:09:15 +0900

    Add README

---
+++ .emacs.d/conf/carbon-emacs.el
@@ -0,0 +1,12 @@
+;; 文字コードを指定する
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+;; 起動時の画面を表示しない
+(setq inhibit-startup-message t)
+(display-time-mode 1)
+(display-battery-mode 1)
+(if window-system (progn
+
+;; カラム番号を表示
+(column-number-mode t)
