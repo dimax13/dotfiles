c65e27706711fea15e95032eb336ac27dc906cfb
dimax13 <dimax13@macsbook-air.local>
2012-10-18 17:44:20 +0900

    os別のファイル名の設定

--- .emacs.d/conf/carbon-emacs.el
+++ .emacs.d/conf/carbon-emacs.el
@@ -1,12 +1,23 @@
-;; 文字コードを指定する
-(set-language-environment "Japanese")
-(prefer-coding-system 'utf-8)
-
 ;; 起動時の画面を表示しない
 (setq inhibit-startup-message t)
 (display-time-mode 1)
 (display-battery-mode 1)
 (if window-system (progn
+
+;; 文字コードを指定する
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+;; Mac OS Xの場合のファイル名の設定
+(when (eq system-type 'darwin)
+  (require 'ucs-normalize)
+  (set-file-name-coding-system 'utf-8-hfs)
+  (setq locale-coding-system 'utf-8-hfs))
+
+;; Windowsの場合のファイル名の設定
+(when (eq window-system 'w32)
+  (set-file-name-coding-system 'cp932)
+  (setq locale-coding-system 'cp932))
 
 ;; カラム番号を表示
 (column-number-mode t)
