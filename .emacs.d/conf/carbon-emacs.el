97257e93015f6e6772a5ea7314eb5d562fbbd063
dimax13 <dimax13@macsbook-air.local>
2012-10-19 19:09:16 +0900

    色と透明度の設定を変更

--- .emacs.d/conf/carbon-emacs.el
+++ .emacs.d/conf/carbon-emacs.el
@@ -1,7 +1,10 @@
 ;; 起動時の画面を表示しない
 ;; (setq inhibit-splash-screen t)
 ;; スタートアップページを表示しない
+;; (setq inhibit-startup-message t)
+;;スプラッシュ(起動画面)抑止
 (setq inhibit-startup-message t)
+(buffer-menu)
 
 ;; 文字コードを指定する
 (set-language-environment "Japanese")
@@ -47,11 +50,32 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 表示・装飾の設定
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 色の設定
+(if window-system
+    (progn
+      (set-background-color "gray2")
+      (set-foreground-color "White")
+      (set-cursor-color "Green")
+      )
+  )
 
-;; 文字の色を設定します。
-(add-to-list 'default-frame-alist '(foreground-color . "gray10"))
-;; 背景色を設定します。
-(add-to-list 'default-frame-alist '(background-color . "white"))
+;; デフォルトの透明度を設定する
+(add-to-list 'default-frame-alist '(alpha . 70))
+
+;; カレントウィンドウの透明度を変更する (85%)
+;; (set-frame-parameter nil 'alpha 0.85)
+(set-frame-parameter nil 'alpha 70)
+
+;;　現在行のハイライト
+'(defface my-hl-line-face
+  '((((class color) (background dark))
+     (:background "NavyBlue" t))
+    (((class color) (background light))
+     (:background "LightGoldenrodYellow" t))
+    (t (:bold t)))
+  "hl-line's my face")
+;(setq hl-line-face 'my-hl-line-face)
+'(global-hl-line-mode 0)
 
 ;; 文字の色
 ;; (add-to-list 'default-frame-alist '(foreground-color . "white"))
@@ -59,3 +83,7 @@
 ;; (add-to-list 'default-frame-alist '(background-color . "black"))
 ;; リージョンの背景色を変更
 (set-face-background 'region "darkgreen")
+
+
+
+
