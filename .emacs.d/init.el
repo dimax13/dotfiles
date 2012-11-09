36cf8d39b016a495658a32b0c187bb4206305e35
dimax13 <dimax13@macsbook-air.local>
2012-11-09 16:05:55 +0900

    autoinstallのコメントアウトで不具合が出ていたので修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,4 @@
+
 ;; Emacs 23より前のバージョンを利用する場合は
 ;; user-emacs-directory 変数が未定義のため次の設定を追加
 (when (< emacs-major-version 23)
@@ -39,14 +40,11 @@
 
 ;; auto-installを追加
 (when (require 'auto-install nil t)
-;; インストールディレクトリを設定する 初期値は ~/.emacs.d/auto-install/
-  (setq auto-install-directory "~/.emacs.d/elisp/")
-;; EmacsWikiに登録されているelisp の名前を取得する
-  (auto-install-update-emacswiki-package-name t)
-;; 必要であればプロキシの設定を行う
-;; (setq url-proxy-services '(("http" . "localhost:8339")))
-;; install-elisp の関数を利用可能にする
-  (auto-install-compatibility-setup))
+  (setq auto-install-directory "~/.emacs.d/elisp/") ; インストールディレクトリを設定する 初期値は ~/.emacs.d/auto-install/
+  (auto-install-update-emacswiki-package-name t) ; EmacsWikiに登録されているelisp の名前を取得する
+;; (setq url-proxy-services '(("http" . "proxy.djedu.kit.ac.jp:8080"))) ; 必要であればプロキシの設定を行う
+  (auto-install-compatibility-setup)) ; install-elisp の関数を利用可能にする
+
 
 ;; auto-completeを追加
 (require 'auto-complete)
