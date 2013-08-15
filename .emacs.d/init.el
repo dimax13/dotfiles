359ec0e34d84e97de9e844ceaf2735501b66ef7d
dimax13 <dimax13@macsbook-air.local>
2013-08-15 10:54:08 +0900

    サブディレクトリが読み込まれない問題を解決 lebel->level

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,33 +4,37 @@
   (defvar user-emacs-directory "~/.emacs.d/"))
 
 ;; load-path を追加する関数を定義
-;; (defun add-to-load-path (&rest paths)
-;;   (let (path)
-;;     (dolist (path paths paths)
-;;       (let ((default-directory
-;; 	      (expand-file-name (concat user-emacs-directory path))))
-;; 	(add-to-list 'load-path default-directory)
-;; 	(if (fboundp 'normal-top-lebel-add-subdirs-to-load-path)
-;; 	    (normal-top-level-add-subdirs-to-load-path))))))
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
 
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
-;;(add-to-load-path "elisp" "conf" "public_repos")
+(add-to-load-path "./elisp" "./conf" "./public_repos")
+;;(add-to-load-path "./elisp")
+;;(add-to-load-path "./conf")
+;;(add-to-load-path "./public_repos")
 
-;; load-path
-(let ((default-directory (expand-file-name "~/.emacs.d/conf")))
-  (add-to-list 'load-path default-directory)
-  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-      (normal-top-level-add-subdirs-to-load-path)))
+;; Loade-path
+;; (let ((default-directory (expand-file-name "~/.emacs.d/conf")))
+;;   (add-to-list 'load-path default-directory)
+;;   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+;;       (normal-top-level-add-subdirs-to-load-path)))
 
-(let ((default-directory (expand-file-name "~/.emacs.d/elisp")))
-  (add-to-list 'load-path default-directory)
-  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-      (normal-top-level-add-subdirs-to-load-path)))
+;; (let ((default-directory (expand-file-name "~/.emacs.d/elisp")))
+;;   (add-to-list 'load-path default-directory)
+;;   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+;;       (normal-top-level-add-subdirs-to-load-path)))
 
-(let ((default-directory (expand-file-name "~/.emacs.d/public_repos")))
-  (add-to-list 'load-path default-directory)
-  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-      (normal-top-level-add-subdirs-to-load-path)))
+;; (let ((default-directory (expand-file-name "~/.emacs.d/public_repos")))
+;;   (add-to-list 'load-path default-directory)
+;;   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+;;       (normal-top-level-add-subdirs-to-load-path)))
+
 
 ;; init-loader.elを追加
 ;; http://coderepos.org/share/browser/lang/elisp/init-loader.el
