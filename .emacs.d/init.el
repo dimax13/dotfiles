142330840f6399004ba9fcc6e5ddbf5934c30f52
dimax13 <dai13.freeman@gmail.com>
2014-03-06 02:04:14 +0900

    find filesのデフォルトディレクトリをホームディレクトリにした

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+﻿;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; emacs起動時に読み込むディレクトリの設定
@@ -10,6 +10,10 @@
 ;; user-emacs-directory 変数が未定義のため次の設定を追加
 (when (< emacs-major-version 23)
   (defvar user-emacs-directory "~/.emacs.d/"))
+
+;; Find Filesで最初からホームディレクトリ以下を探しに行くようにする
+(cd "~/")
+
 
 ;; load-path を追加する関数を定義
 (defun add-to-load-path (&rest paths)
@@ -23,6 +27,7 @@
 
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
 (add-to-load-path "./elisp" "./conf" "./public_repos")
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; init-loader.elを追加
