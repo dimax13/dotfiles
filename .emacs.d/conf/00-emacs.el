0c23e1f52d97175ced2556398a3672a34d871dd4
dimax13 <dai13.freeman@gmail.com>
2014-03-09 08:52:02 +0900

    recentf-extを追加

--- .emacs.d/conf/00-emacs.el
+++ .emacs.d/conf/00-emacs.el
@@ -37,7 +37,23 @@
 (require 'auto-complete-config)    ; 必須ではないですが一応
 (global-auto-complete-mode t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; recentf-ext導入
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 自動保存
+(when (require 'recentf-ext nil t)
+  (setq recentf-max-saved-items 2000)
+  (setq recentf-exclude '(".recentf"))
+  (setq recentf-auto-cleanup 10)
+  (setq recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list))
+  (recentf-mode 1))
 
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;; 起動画面で recentf を開く
+(add-hook 'after-init-hook (lambda()
+    (recentf-open-files)
+    ))
 
 
 
